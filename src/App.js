67b2f3c1e14bfd60d1aaddeac661990fcd7d4c12
Hongjie Huang <hongjie824@gmail.com>
2019-03-11 01:29:51 -0400

    initialize the files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import People from './People_list/People_list.js';
 class App extends Component {
-  render() {
-    return (
+/*   state={
+  } */
+  
+  render(){
+    return(
+      <div className="App">
+        <Container>
+        <header className="App-header text-center">
+        <h1>Address Book App</h1>
+        </header>
+        </Container>
+  
 
-    );
+      </div>
+    )
   }
 }
-
 export default App;
---
+++ src/People_list/People_list.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
+
+function People_List (props) {
+    let peoples = [...props.peoples];
+}

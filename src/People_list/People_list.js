73d9fd6d324698db55f28b0420d3cc1673b68726
Hongjie Huang <hongjie824@gmail.com>
2019-03-12 16:41:53 -0400

    add to people-list based on react-todo-app

--- src/People_list/People_list.js
+++ src/People_list/People_list.js
@@ -2,6 +2,26 @@ import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Card from 'react-bootstrap/Card';
 
-function People_List (props) {
+
+function people_list (props) {
     let peoples = [...props.peoples];
-}
+    let lists = peoples.map( (list) =>
+        <ListGroup.Item key={list.key}>
+           <Card className="bg-light border rounded">
+           <span key={list.key}
+                className="text-right"
+                onClick={props.closer.bind(null,list.key)}>{'\u274e'}</span>
+           <Card.Body className="text-left">
+       
+           <Card.Title>{list.FirstName} {list.LastName}</Card.Title>
+                <Card.Text>{list.Birthday}<br />{list.Telephone}</Card.Text>
+                </Card.Body>
+            </Card>
+        </ListGroup.Item>
+        
+    )
+    return lists;
+};
+
+
+export default people_list;
